108565cc747cff713341e77f807f781fe163bfe6
Rooban Samuel <rooban24samuel@gmail.com>
2023-11-17 18:10:03 +0530

    Initial commit

---
+++ project_ems/project_ems/doctype/event_creation/test_event_creation.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Rooban Samuel and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestEventcreation(FrappeTestCase):
+	pass
